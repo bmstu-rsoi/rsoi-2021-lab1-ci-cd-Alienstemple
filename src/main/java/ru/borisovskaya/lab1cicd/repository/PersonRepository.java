9dc303b003aa89ba58b11b01becac5e8da7689d9
Krautkraft <borisovsk0101@gmail.com>
2021-10-30 17:48:18 +0300

    clean to merge with develop

--- src/main/java/ru/borisovskaya/lab1cicd/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.borisovskaya.lab1cicd.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.borisovskaya.lab1cicd.model.Person;
-
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-}
